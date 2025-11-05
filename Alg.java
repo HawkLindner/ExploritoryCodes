273d9088a6621d85702f97828e55be9c0f8219d4
HawkLindner <hm29lind@siena.edu>
2025-11-05 18:16:17 -0500

    Working on sorting alg in java

---
+++ Alg.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+public class Alg {
+    public static int[] sort(int[] arr){
+        for(int i = 0 ; i < arr.length - 1 ; i++){
+            for(int j = 0 ; j < arr.length - i - 1 ; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static int BinarySearch(int[]arr, int target){
+        //Start with protecting against empty array
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Target not found in array");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[50];
+        Random rand = new Random();
+        for(int i = 0 ; i < 10; i++){
+            arr[i] =  rand.nextInt(1,10);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int target = rand.nextInt(1,10);
+        System.out.println("Target: " + target);
+        try {
+            int index = BinarySearch(arr, target);
+            System.out.println("Target found at index: " + index);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int arr1Sorted[] = arr;
+        arr1Sorted = sort(arr1Sorted);
+        System.out.print("Sorted array: ");
+        for(int i = 0 ; i < arr1Sorted.length ; i++){
+            System.out.print(arr1Sorted[i] + " ");  
+        }
+        System.out.println();
+    }
+}
